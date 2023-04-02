c90a5ea413eeae4f4de48748515a7d26690ca386
Jasleenkaur1998 <91931824+Jasleenkaur1998@users.noreply.github.com>
2023-04-01 19:01:48 -0700

    user login page UI

--- src/pages/user/login/login.jsx
+++ src/pages/user/login/login.jsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Box } from "@mui/material";
+import classes from "./login.module.css";
+import CustomButton from "../../../components/button/Button";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+
+ const [email, setEmail] = useState("");
+ const [password, setPassword] = useState("");
+
+ // React router gives a hook where we can programmatically send a user to different page
+ const navigate = useNavigate();
+
+  const loginButtonClicked = () => {
+    console.log(email);
+    console.log(password);
+    navigate('/user/home');
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <div>login</div>
-  )
+    <Box
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      gap="20px"
+      backgroundColor="aquamarine"
+    >
+      <h1>Welcome, You may login here!</h1>
+      <TextField
+        type="email"
+        label="Email"
+        variant="outlined"
+        className={classes.textFields}
+        placeholder="Enter your email"
+        required
+        onChange={handleEmail}
+      />
+      <TextField
+        type="password"
+        label="Password"
+        variant="outlined"
+        className={classes.textFields}
+        placeholder="Enter your Password"
+        required
+        onChange={handlePassword}
+      />
+
+      <Link to="/user/signup">Don't have an account ?</Link>
+
+      <CustomButton
+        title="Sign in"
+        variant="contained"
+        handleClick={loginButtonClicked}
+        color="warning"
+        styleConfig={{
+          width: "300px",
+        }}
+      />
+    </Box>
+  );
 }
